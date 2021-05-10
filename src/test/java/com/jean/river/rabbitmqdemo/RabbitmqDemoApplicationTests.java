c5fba587aa9575fff8a457a30e2c1f3be2e0020e
yamakaze <xiaoliqiwen@gmail.com>
2021-05-10 08:50:15 +0800

    Initial commit

---
+++ src/test/java/com/jean/river/rabbitmqdemo/RabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.river.rabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
